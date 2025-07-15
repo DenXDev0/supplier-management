2c0782d3ef828909aa1d7d6cca37a470740de3d9
Deni Anggara <DenxDev@gmail.com>
2025-07-15 14:07:50 +0700

    Update SupplierCards.tsx

--- src/pages/SupplierList/SupplierCards.tsx
+++ src/pages/SupplierList/SupplierCards.tsx
@@ -3,7 +3,7 @@ import { Card, Flex } from 'antd';
 import { cardData } from './data';
 
 const SupplierCards: React.FC = () => (
-  <Flex justify="center" wrap gap="middle">
+  <Flex justify="space-between" wrap gap="middle">
     {cardData.map(({ title, value, icon, change }, i) => (
       <Card key={i} title={title} variant="borderless" style={{ width: 280 }} size="small">
         <Flex justify="space-between" style={{ fontSize: '1.5rem', marginBottom: '0.5rem' }}>
